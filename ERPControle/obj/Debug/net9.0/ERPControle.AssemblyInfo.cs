c4eb7a227b883d400939b4f906966f477b1df3bc
João Vítor Santos Lima <jvitor.ti03@gmail.com>
2025-02-17 18:40:03 -0300

    feat: project finished

--- ERPControle/obj/Debug/net9.0/ERPControle.AssemblyInfo.cs
+++ ERPControle/obj/Debug/net9.0/ERPControle.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERPControle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd6266f774b569e655940ba2953afaa653e9150d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7addefaf72cb8d35e387fc18dadc4448bf2671c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERPControle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERPControle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
